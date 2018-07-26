5a9e71c731e67b41e93abb0bd13ab0beb7172acc
tensopatrao <41700803+tensopatrao@users.noreply.github.com>
2018-07-26 07:15:07 +0100

    s

--- main3.js
+++ main3.js
@@ -15,7 +15,7 @@ https.createServer(options,function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
 	
 	 // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://launch.playcanvas.com:443');
+    res.setHeader('Access-Control-Allow-Origin', 'https://launch.playcanvas.com:443'); 
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
