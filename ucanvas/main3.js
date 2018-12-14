e5c7ba19c6c4bdd19550427c29aceae5990749bb
tensopatrao <41700803+tensopatrao@users.noreply.github.com>
2018-12-14 06:01:00 +0000

    Update main3.js

--- ucanvas/main3.js
+++ ucanvas/main3.js
@@ -79,7 +79,7 @@ io.sockets.on('connection', function (socket) {
 	
 	if(data!="UNITY-SERVER"){
 		socket.emit('welcome',packageIds, packageParents);
-		io.sockets[unityId].emit('newPlayer', socket.id);
+		io.sockets.socket(unityId).emit('newPlayer', socket.id);
 	}
 	else{
 		unityId=socket.id;
