f8abf98d47e6ab9cedf0fb14e0cd9818e8cdf6a7
tensopatrao <41700803+tensopatrao@users.noreply.github.com>
2018-12-14 19:18:51 +0000

    ds

--- ucanvas/main3.js
+++ ucanvas/main3.js
@@ -100,6 +100,7 @@ io.sockets.on('connection', function (socket) {
   
   socket.on('playerJoined', function(data,data2){
 	  io.sockets.connected[uPlayers[parseInt(data)]].emit('playerJoined', data2);
+	  console.log(uPlayers[parseInt(data)]);
   });
   
   
