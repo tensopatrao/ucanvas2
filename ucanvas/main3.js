73bf6edd65b3132248a2be52659a40c257e25506
tensopatrao <41700803+tensopatrao@users.noreply.github.com>
2018-12-18 07:39:58 +0000

    dsa

--- ucanvas/main3.js
+++ ucanvas/main3.js
@@ -103,7 +103,7 @@ io.sockets.on('connection', function (socket) {
   
   
   socket.on('playerJoined', function(data,data2){
-	  io.sockets.connected[uPlayers[parseInt(data)]].emit('playerJoined', parseInt(data)-1,data2); //SAO PRECISOS OS PARSE INTS?! TESTAR.
+	  io.sockets.connected[uPlayers[parseInt(data)]].emit('playerJoined', parseInt(data),data2); //SAO PRECISOS OS PARSE INTS?! TESTAR.
 	  console.log("Player Joined");
   });
   
